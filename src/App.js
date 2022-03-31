88d305919777a5011d81b75d9209dddbadac743b
Mostafa shawky <mostafashawky20177@gmail.com>
2022-03-31 12:20:25 +0200

    update navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar  from "./component/navbar/Navbar";
 import Login from "./component/login/Login";
 import Signup from "./component/signup/Signup";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import FooterComp from "./component/footer/Footer";
-=======
-import Navbar  from "./component/navbar/Navbar";
-
->>>>>>> test
-=======
-import Navbar  from "./component/navbar/Navbar";
-
->>>>>>> parent of 7e65809 (update navbar,footer)
 
 function App() {
   return (
@@ -22,6 +13,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
+        <FooterComp />
       </BrowserRouter>
     </>
   );
