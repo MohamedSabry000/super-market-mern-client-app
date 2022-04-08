faa3809b57156ac4956a47b65a1240f2c45254e8
nehalmohsen22 <nehalmoh27@gmail.com>
2022-04-08 05:28:24 +0200

    about us

--- src/component/aboutus/AboutUs.jsx
+++ src/component/aboutus/AboutUs.jsx
@@ -10,19 +10,19 @@ function AboutUss(props) {
     <>
       <Navbar />
       <MainSection />
+      <p className="team">Meet The Team</p>
+
       <div className="wrapper">
         <Card
           img="https://images.unsplash.com/photo-1536304929831-ee1ca9d44906?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
           title="Mohamed "
           description="Full Stack Developer"
         />
-
         <Card
           img="https://images.unsplash.com/photo-1476124369491-e7addf5db371?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
           title="Nehal "
           description="Full-Stack-Developer"
         />
-
         <Card
           img="https://images.unsplash.com/photo-1529928520614-7c76e2d99740?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
           title="Mostafa "
